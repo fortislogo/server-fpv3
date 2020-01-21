eeb46e92d1ce848e20ba9b761733fd4118e06fbd
Celso N Malacas Jr <cmalacas@rocketsource.co>
2020-01-21 21:15:03 +0800

    update db

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql2');
+const db = require('../dbWrapper');
 var multer = require("multer");
 const fs = require('fs');
 var nodemailer = require("nodemailer")
@@ -11,13 +11,13 @@ var sgTransport = require('nodemailer-sendgrid-transport');
      password: ''
  });
 
-connection.connect(function (err) {
+db.connect(function (err) {
   if (!err) {
       console.log("Local Database is connected to local db");
   } else {
       console.log("Error connecting Local database local db", err);
   }
-});*/
+});
 
 connection = mysql.createConnection({
     host: 'arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
@@ -27,18 +27,18 @@ connection = mysql.createConnection({
 });
 
 
-connection.connect(function (err) {
+db.connect(function (err) {
     if (!err) {
         console.log("Local Database is connected to flatpacksdb");
     } else {
         console.log("Error connecting Local database flatpacksdb", err);
     }
-});
+});*/
 
 exports.login = async(req, res) => {
   var email = req.body.email;
   var mobile = req.body.mobile;
-  connection.query('SELECT * FROM customer WHERE Email = ? AND MobileNumber = ?', [email, mobile], function(err, rows) {
+  db.query('SELECT * FROM customer WHERE Email = ? AND MobileNumber = ?', [email, mobile], function(err, rows) {
     if (!err) {
       if (rows.length > 0) {
           res.send({
@@ -63,7 +63,7 @@ exports.register = async(req, res) => {
   var mobile = req.body.mobile;
   var name = req.body.name;  
 
-  connection.query("SELECT * FROM customer WHERE email = ?", [email], function(err, rows) {
+  db.query("SELECT * FROM customer WHERE email = ?", [email], function(err, rows) {
     if (!err) {
       if (rows.length > 0) {
         res.send({
@@ -72,7 +72,7 @@ exports.register = async(req, res) => {
         });
       } else {
 
-        connection.query("INSERT INTO customer (Name, Email, MobileNumber, CreatedDate) VALUES (?, ?, ?, NOW())", [name, email, mobile], function(err, rows) {
+        db.query("INSERT INTO customer (Name, Email, MobileNumber, CreatedDate) VALUES (?, ?, ?, NOW())", [name, email, mobile], function(err, rows) {
 
           if (!err) {
             res.send({
@@ -91,7 +91,7 @@ exports.register = async(req, res) => {
 exports.getCustomer = async(req, res) => {
   var customerId = req.body.id; 
 
-  connection.query("SELECT * FROM customer WHERE CustomerId = ?", [customerId], function(err, rows) {
+  db.query("SELECT * FROM customer WHERE CustomerId = ?", [customerId], function(err, rows) {
     if (!err) {
       if (rows.length > 0) {
         res.send({
@@ -102,3 +102,4 @@ exports.getCustomer = async(req, res) => {
     }
   });
 };
+
